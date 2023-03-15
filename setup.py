27826d3038923b12118f13b791db5b3d28c5a167
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-03-15 04:44:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='flashdetect',
-    version='0.1',
-    description='A package for analyzing videos for flashing lights and contrasting light and dark patterns',
-    url='https://github.com/LoopGlitch26/FlashDetect',
+    version='1.0.0',
     author='Bravish Ghosh',
     author_email='bravish.ghosh@outlook.com',
-    license='MIT',
+    description='Detects flashing lights and contrasting patterns in videos',
     packages=find_packages(),
     install_requires=[
-        'pytube',
         'opencv-python',
         'numpy',
-        'matplotlib'
+        'matplotlib',
+        'pytube',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    keywords='video analysis flashing lights contrast',
 )
